e5cac4c3aa4aa62493b69b58e6f684c88808f53c
Illia16 <illia2143@gmail.com>
2021-09-05 01:05:34 -0400

    logic completed

--- pages/bonus.vue
+++ pages/bonus.vue
@@ -14,25 +14,13 @@
         </button>
 
         <div class="flex space-x-10">
-            <div class="glass glass-1">
-                <div
-                    :class="['glass-inner glass-1-inner']"
-                    :style="{ height: glass1 ? glass1 + 'px' : '0' }"
-                ></div>
-            </div>
-
-            <div class="glass glass-2">
-                <div
-                    :class="['glass-inner glass-2-inner']"
-                    :style="{ height: glass2 ? glass2 + 'px' : '0' }"
-                ></div>
-            </div>
-
-            <div class="glass glass-3">
-                <div
-                    :class="['glass-inner glass-3-inner']"
-                    :style="{ height: glass3 ? glass3 + 'px' : '0' }"
-                ></div>
+            <div v-for="(glassValue, glass, index) in glasses" :key="index" class="text-white">
+                <div :class="[`glass glass-${index}`]">
+                    <div
+                        :class="[`glass-inner glass-${index}-inner`]"
+                        :style="{ height: glassValue ? glassValue + 'px' : '0' }"
+                    ></div>
+                </div>
             </div>
         </div>
     </div>
@@ -43,9 +31,13 @@ declare interface MiniGame {
     isMouseDown: boolean;
     currentGlass: number;
     totalAmount: number;
-    glass1: number;
-    glass2: number;
-    glass3: number;
+    userAmount: number;
+    glasses: {
+        glass1: number,
+        glass2: number,
+        glass3: number,
+    };
+    gameOver: boolean,
 }
 
 import Vue from "vue";
@@ -54,47 +46,73 @@ export default Vue.extend({
         return {
             isMouseDown: false,
             currentGlass: 1,
+            userAmount: 120,
             totalAmount: 120,
-            // glasses: {
-            //     glass1: 0,
-            //     glass2: 0,
-            //     glass3: 0
-            // },
-            glass1: 0,
-            glass2: 0,
-            glass3: 0
+            glasses: {
+                glass1: 0,
+                glass2: 0,
+                glass3: 0
+            },
+            gameOver: false,
         };
     },
-    mounted: () => {
+    mounted: function() {
         console.log(this);
     },
     methods: {
         pushDown(e: KeyboardEvent) {
+            if (this.totalAmount === 0) {
+                return
+            };
+
             if (e.type === "mousedown") {
                 this.isMouseDown = true;
-                console.log(this.isMouseDown);
                 let timer = setInterval(() => {
                     this.totalAmount = this.totalAmount - 5;
                     this.getCurrentGlass(this.currentGlass);
                     if (!this.isMouseDown) {
-                        this.currentGlass++;
+                        if (this.currentGlass === 3){
+                            this.currentGlass = 1;
+                        } else {
+                            this.currentGlass++;
+                        }
                         clearInterval(timer);
                     }
-                }, [50]);
+
+                    if (this.totalAmount === 0) {
+                        clearInterval(timer);
+                    };
+                }, [100]);
             } else if (e.type === "mouseup") {
                 this.isMouseDown = false;
-                console.log(this.isMouseDown);
             }
         },
         getCurrentGlass(glassNum: number): void {
-            this[`glass${glassNum}`] = this[`glass${glassNum}`] + 5;
+            this.glasses[`glass${glassNum}`] = this.glasses[`glass${glassNum}`] + 5;
         }
     },
     watch: {
-        totalAmount: function(val, oldVal) {
-            console.log("val", val);
+        totalAmount: function() {
+            console.log('total', this.totalAmount);
+
+            if (this.glasses.glass1 && this.glasses.glass2 && this.glasses.glass3 === this.userAmount/3) {
+                console.log('WIN!');
+                this.gameOver = true;
+            } else if ((this.glasses.glass1 || this.glasses.glass2 || this.glasses.glass3 !== this.userAmount/3) && (this.glasses.glass1 && this.glasses.glass2 && this.glasses.glass3 !== 0))  {
+                console.log('LOOSE!');
+            }
+
+
+            // if (this.totalAmount === 0) {
+            //     document.querySelector('button').removeEventListener('mousedown')
+            // }
         }
-    }
+    },
+    // watch: {
+    //     totalAmount: function(val, oldVal) {
+    //         console.log("val", val);
+    //     }
+    // }
 });
 </script>
 
@@ -103,7 +121,7 @@ export default Vue.extend({
     @apply bg-black;
 
     .glass {
-        @apply flex items-end w-32 h-44 border-2;
+        @apply flex items-end w-32 h-[120px] border-2;
         .glass-inner {
             @apply w-full bg-red-700;
             transition: height 0.3s ease;
